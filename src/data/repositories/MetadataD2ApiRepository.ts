f1912455083b3379db61b8ea96e22bd8c42343f4
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-07-09 20:03:31 +0800

    fix: fix default handling

--- src/data/repositories/MetadataD2ApiRepository.ts
+++ src/data/repositories/MetadataD2ApiRepository.ts
@@ -79,15 +79,15 @@ export class MetadataD2ApiRepository implements MetadataRepository {
     }
 
     public getModelName(model: string): string {
-        return this.api.models[model as ModelIndex]?.schema?.displayName ?? i18n.t("Unknown model");
+        return this.api.models[model as ModelIndex]?.schema.displayName ?? i18n.t("Unknown model");
     }
 
     public isShareable(model: string): boolean {
-        return this.api.models[resolveModelAlias(model)].schema.shareable ?? false;
+        return this.api.models[resolveModelAlias(model)]?.schema.shareable ?? false;
     }
 
     public isDataShareable(model: string): boolean {
-        return this.api.models[resolveModelAlias(model)].schema.dataShareable ?? false;
+        return this.api.models[resolveModelAlias(model)]?.schema.dataShareable ?? false;
     }
 
     private _fetchMetadata(ids: string[]): FutureData<MetadataPayload> {
